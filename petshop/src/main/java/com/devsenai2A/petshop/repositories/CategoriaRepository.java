ce75bad8690967761b3bd2d1b8f3800658668674
mabernardii <manuela.bernardi1a@gmail.com>
2026-03-31 14:07:22 -0300

    Delete petshop directory

--- petshop/src/main/java/com/devsenai2A/petshop/repositories/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devsenai2A.petshop.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.devsenai2A.petshop.entities.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
